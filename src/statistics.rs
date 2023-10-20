721ba8e7cb71eede2950ff6541bef31a8a3b20f3
Alysson-Alves23 <alyssonja23@gmail.com>
2023-10-19 22:03:07 -0300

    Feature: functions assigns defined

---
+++ src/statistics.rs
@@ -0,0 +1,63 @@
+pub mod statistics{
+    pub fn mean<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+    }
+    pub fn geometric_mean<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+    }
+    pub fn harmonic_mean<T: Into<f64>>(x: &[T]) -> f64{
+
+        return 0.0;
+    }
+
+    pub fn median<T: Into<f64>>(x: &[T]) -> f64{
+    return 0.0;
+    }
+    pub fn median_low<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+    }
+    pub  fn median_high<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+    }
+    pub fn median_grouped<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+    }
+    // pub fn mode<T: Into<f64>>(x: &[T]) -> f64{
+    //     return 0.0;
+    // }
+    // pub fn multimode<T: Into<f64>>(x: &[T]) -> f64{
+    //     return 0.0;
+    // }
+    pub fn pstdev<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn pvariance<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn stdev<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn variance<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn quantiles<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn covariance<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn correlation<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+    pub fn linear_regression<T: Into<f64>>(x: &[T]) -> f64{
+        return 0.0;
+
+    }
+}
