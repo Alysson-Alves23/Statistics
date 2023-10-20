79b7e0e606c38e911735c4ca7597ea24b5c8276e
Alysson-Alves23 <alyssonja23@gmail.com>
2023-10-19 23:57:47 -0300

    Feature: 'mean()' implementation

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,7 +1,32 @@
 pub mod statistics{
-    pub fn mean<T: Into<f64>>(x: &[T]) -> f64{
-        return 0.0;
+
+    use std::iter::Sum;
+
+    trait Numeric {
+        fn to_f64(&self) -> f64;
     }
+
+    impl Numeric for i32 {
+        fn to_f64(&self) -> f64 {
+            *self as f64
+        }
+    }
+
+    impl Numeric for f64 {
+        fn to_f64(&self) -> f64 {
+            *self
+        }
+    }
+
+    pub fn mean<T>(x: &[T]) -> f64
+        where
+            T: Numeric,
+    {
+        let count = x.len() as f64;
+        x.iter().map(|val| val.to_f64()).sum() / count
+    }
+
+
     pub fn geometric_mean<T: Into<f64>>(x: &[T]) -> f64{
         return 0.0;
     }
